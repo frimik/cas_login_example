199f68ddee17e15f5628517142369cc49ed508bb
Marlon Moyer <marlon.moyer@acig.com>
2013-03-21 14:26:30 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CasLogin::Application.config.secret_token = '32a0e2cfe6f40dd9f1fb8d61c06cef38c1a10d70556e5c749a5ae1fc74758d8b49e2779a07caa6f63562999400ffd2a91b03bd3ffe15423a7eb2ad73219522bc'
